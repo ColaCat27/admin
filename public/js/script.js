a00ada3a30b997792c8e54d93c09aa4a901223a6
Vladislav <vlad.tiw27@gmail.com>
2021-03-13 13:11:09 +0200

    написал добавления товара в базу данных

--- public/js/script.js
+++ public/js/script.js
@@ -5,8 +5,12 @@ window.addEventListener('DOMContentLoaded', () => {
 const greetings = document.querySelector('form[data-form="greetings"]');
 const about = document.querySelector('form[data-form="about"]');
 const events = document.querySelector('form[data-form="events"]');
+const shop = document.querySelector('.shop__form');
 
-function sendData(ur, form) {
+function sendData(ur, form, translit = false) {
+    if (translit) {
+        
+    }
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         let formData = new FormData(form);
@@ -20,7 +24,7 @@ function sendData(ur, form) {
         })
     });
 
-    async function postData(url, data) {
+    async function postData(url, data = '') {
         const result = await fetch(url, {
             method: 'POST',
             body: data
@@ -32,5 +36,44 @@ function sendData(ur, form) {
 sendData('/greetings', greetings);
 sendData('/about', about);
 sendData('/events', events);
+
+sendData('/upload', shop);
+
+function getData(url) {
+    async function getInfo(u) {
+        const result = await fetch(u, {
+            method: 'POST'
+        })
+        return result;
+    }
+
+    getInfo(url)
+    .then(resp => {
+        return resp.json()
+    })
+    .then(info => {
+        useData(info);
+    })
+    .catch(e => {
+        console.log(e);
+    })
+}
+
+getData('/getinfo');
+
+function useData(d) {
+const greetings = document.querySelector('form[data-form="greetings"]');
+const about = document.querySelector('form[data-form="about"]');
+const events = document.querySelector('form[data-form="events"]');
+
+greetings.querySelector('input').value = d.greetings;
+about.querySelector('input').value = d.about;
+events.querySelector('input').value = d.events;
+
+}
+
+
+
+
 
 });
